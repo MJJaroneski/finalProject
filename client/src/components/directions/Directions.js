aebaa2308f6be50b8c85b2657e38ec9f613eaa43
Chelsea Crooke <chelsea.crooke@yahoo.com>
2020-01-15 21:52:07 -0500

    directions css added, still need button and hidden page linked

--- client/src/components/directions/Directions.js
+++ client/src/components/directions/Directions.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Container } from "../Grid";
 import "./addressProtect.css"
-
 class Directions extends React.Component {
+  handleClick(){
+    console.log('this is:', this);
+  }
   render() {
     return (
-      <div>Directions</div>
+      <body className="directBody">
+      
+      <Container>
+          <h1 className="display-4">Lucy Lane Address Page</h1>
+            <p className="lead">We do not give out the Lucy Lane address publicly. It is our home after all. If you’d like to come to a show,fill out <a className="formLink" href="../html/contact.html">this form</a> and we’ll send you a password. Once you receive the password, click the button below and you’ll be able to access the address and all directions 8:00am the day of the show. You will not be able to access it after the show is over and the password changes every show.</p>
+              <button className="btn btn-primary btn-lg passwordBtn" href="#" onClick={(e) => this.handleClick(e)}>Enter Protected Area</button>
+              <br></br>
+      </Container>
+      
+      </body>
     );
   }
 }
-
 Directions.propTypes = {
   text: PropTypes.string.isRequired
 };
